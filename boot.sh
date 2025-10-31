49e4823f8e19ab3b7076b9146bb0f60454ddddfc
VirtualMachineMaker <b480132@outlook.com>
2025-10-30 20:39:50 -0700

    Create boot.sh

---
+++ boot.sh
@@ -0,0 +1,69 @@
+chmod +x ./windows/kubernetes.yml
+chmod +x ./windows/compose.yml
+chmod +x ./windows/Dockerfile
+chmod +x ./windows/.dockerignore
+chmod +x ./windows/src/define.sh
+chmod +x ./windows/src/samba.sh
+chmod +x ./windows/src/power.sh
+chmod +x ./windows/src/mido.sh
+chmod +x ./windows/src/install.sh
+chmod +x ./windows/src/entry.sh
+chmod +x ./windows/assets/win.exe
+chmod +x ./windows/assets/win10x64-enterprise-eval.xml
+chmod +x ./windows/assets/win10x64-enterprise.xml
+chmod +x ./windows/assets/win10x64-iot.xml
+chmod +x ./windows/assets/win10x64-ltsc.xml
+chmod +x ./windows/assets/win10x64.xml
+chmod +x ./windows/assets/win11x64-enterprise-eval.xml
+chmod +x ./windows/assets/win11x64-enterprise.xml
+chmod +x ./windows/assets/win11x64-iot.xml
+chmod +x ./windows/assets/win11x64-ltsc.xml
+chmod +x ./windows/assets/win11x64.xml
+chmod +x ./windows/assets/win2008r2-eval.xml
+chmod +x ./windows/assets/win2008r2.xml
+chmod +x ./windows/assets/win2012r2-eval.xml
+chmod +x ./windows/assets/win2012r2.xml
+chmod +x ./windows/assets/win2016-eval.xml
+chmod +x ./windows/assets/win2016.xml
+chmod +x ./windows/assets/win2019-eval.xml
+chmod +x ./windows/assets/win2019-hv.xml
+chmod +x ./windows/assets/win2019.xml
+chmod +x ./windows/assets/win2022-eval.xml
+chmod +x ./windows/assets/win2022.xml
+chmod +x ./windows/assets/win2025-eval.xml
+chmod +x ./windows/assets/win2025.xml
+chmod +x ./windows/assets/win7x64-enterprise-eval.xml
+chmod +x ./windows/assets/win7x64-enterprise.xml
+chmod +x ./windows/assets/win7x64-ultimate.xml
+chmod +x ./windows/assets/win7x64.xml
+chmod +x ./windows/assets/win7x86-enterprise.xml
+chmod +x ./windows/assets/win7x86-ultimate.xml
+chmod +x ./windows/assets/win7x86.xml
+chmod +x ./windows/assets/win81x64-enterprise-eval.xml
+chmod +x ./windows/assets/win81x64-enterprise.xml
+chmod +x ./windows/assets/win81x64.xml
+chmod +x ./linux/.editorconfig
+chmod +x ./linux/Dockerfile
+chmod +x ./linux/Dockerfile.aarch64
+chmod +x ./linux/Jenkinsfile
+chmod +x ./linux/jenkins-vars.yml
+chmod +x ./linux/compose.yml
+chmod +x ./linux/package_versions.txt
+chmod +x ./linux/root/defaults/startwm.sh
+chmod +x ./linux/defaults/xfce/xsettings.xml
+chmod +x ./linux/root/defaults/xfce/xfwm4.xml
+chmod +x ./linux/root/defaults/xfce/xfce4-panel.xml
+chmod +x ./linux/root/defaults/xfce/xfce4-desktop.xml
+chmod +x ./linux/
+chmod +x ./linux/
+chmod +x ./linux/
+chmod +x ./linux/
+chmod +x ./linux/
+
+
+
+
+
+
+
+
