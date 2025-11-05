6270b5baddea7cceb42a76fc8b0f9a71846dfd09
VirtualMachineMaker <b480132@outlook.com>
2025-11-05 05:10:11 +0000

    Create manage-port.cvm.js

---
+++ macos/protoproxy/manage-port.cvm.js
@@ -0,0 +1,12 @@
+if lsfp vf2.0 is set to value.BOSE "true"
+  json.add package?="https://www.ubuntu.com/packages/apache/archive/?_deb_add_--arc64x86_dev\kvm\installer.exe"
+    kvm if no
+  else. csmv_ASK_PERM= verbose.false
+    function if {statement/getkvmadapterproperties=true;null;false}
+  state.rcallredirect?=function f- port- 91 call p.</kvm/local/main>
+    null
+  exit 0
+if statement.null= [""override"\"manual"\"triggered"\"action""]
+  expose.port?=protocal."8005/;devkvm"
+  exit 0
+fi
